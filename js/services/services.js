33e7c15462714aee153423347f923951992606b7
Aleks <i.aleks@tut.by>
2024-05-10 18:51:11 +0300

    modules

---
+++ js/services/services.js
@@ -0,0 +1,24 @@
+// Функция отправки формы
+const postData = async (url, data) => {
+   const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+         'Content-type': 'application/json'
+      },
+      body: data
+   });
+
+   return await res.json();
+};
+
+//функция получения данных
+const getResource = async (url) => {
+   const res = await fetch(url);
+   if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+   }
+   return await res.json();
+};
+
+export { postData, getResource };
+
